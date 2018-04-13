a348b0c18eada28521700f4ba6a744092369665b
Song Zhao <lanmovii@gmail.com>
2018-04-12 22:51:32 -0500

    day1

---
+++ textImage.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import argparse
+
+#命令行输入参数处理
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file')    #input file
+parser.add_argument('-o', '--output')   #optional output condition
+parser.add_argument('--width', type = int, default = 80) #optional width parameter
+parser.add_argument('--height', type = int, default = 80) #optional hight parameter
+
+args = parser.parse_args()
+
+IMG = args.file
+WIDTH = args.width
+HEIGHT = args.height
+OUTPUT = args.output
+
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+
+#use 70 char to display 265 grey scale
+def get_char(r,g,b,alpha = 256):
+    if alpha == 0:
+        return ' '
+    length = len(ascii_char)
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)  #RBG to grey scale formula
+
+    unit = (256.0 + 1)/length
+    return ascii_char[int(gray/unit)]
+
+if __name__ == '__main__':
+    im = Image.open(IMG)
+    im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
+    txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j,i)))  #iterate through each pixel in the picture
+        txt += '\n'
+
+    print txt
+
+    #output the txt string to output file.
+    if OUTPUT:
+        with open(OUTPUT,'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt",'w') as f:
+            f.write(txt)
